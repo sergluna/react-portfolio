a5d3a6a2469e5e9ab7c630f56fc1f33f8f14d566
Moon Man <111019074+sergluna@users.noreply.github.com>
2023-03-12 21:45:29 -0500

    Creating componets and js files

---
+++ portfolio/src/componets/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../style.css'
+
+function Footer() {
+    return (
+        <footer class="footer" id="contact">
+            <a href="https://github.com/sergluna">GitHub</a>
+            <a href="#">Email</a>
+            <a href="#">LinkedIn</a>
+        </footer>
+    )
+}
